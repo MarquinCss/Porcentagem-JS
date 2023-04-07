c7bc8116aae1176e215e716c48d0ab78bf641fe0
Marco Marchi <115740827+MarquinCss@users.noreply.github.com>
2023-04-06 23:46:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
-function enviar() {
-  var sb = document.getElementById("sb").value;
-  var desc = document.getElementById("desc").value;
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Porcentagem Desconto Sálario JS</title>
+</head>
+<body>
+<!--ideia inicial é construir uma calculadora dessas que mostra os descontos (o que foi descontado, me inspirando na aula de extensão da professora Clarice-->
+    <div class="container">
+        <H1>Calculadora de desconto percentual de Salário</H1>
+<input id="sb" placeholder="Salário Bruto" type="number">
 
-  var liquido = sb - (sb * desc / 100);
+<input id="desc" placeholder="Descontos(%) SEM SINAL DE %" type="text">
 
-  var desconto = liquido - sb
+<button onclick="enviar()" class="resultado">Enviar</button>
 
-  document.querySelector(".msg").innerHTML = `O valor líquido é de R$ ${liquido.toFixed(2)} e é descontado do seu salário todo mês R$ ${desconto}.`;
-  
-  /*tofixed limita os decimais de acordo com o valor que colocamos nos parenteses ( ) 
-  
-  Exemplo:
-  
-  const numero = 3.14159265359;
-  
-const numeroFormatado = numero.toFixed(2);
+<div class="msg"></div>
 
-console.log(numeroFormatado); // retorna 3.14
-  */
-}
+<br><br>
+Por Marco Marchi
+</div>
+<script src="main.js"></script>
+</body>
+</html>
